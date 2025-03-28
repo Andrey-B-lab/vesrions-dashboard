55bd89a35135a4f5c46f72f4ded7260b43aa31b2
Andrey B <72738126+Andrey-B-lab@users.noreply.github.com>
2025-03-28 14:07:52 +0300

    Create insert-fake-data.sql

---
+++ PostgreSQL/insert-fake-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO deployed_versions (
+  job_name,
+  version,
+  environment,
+  deployed_at,
+  system,
+  deployed_by
+) VALUES
+  ('טרייד אתר', '2.0.3', 'DEV',    '2025-03-28 11:17:20', 'leumi-trade', 'Yael N'),
+  ('אתר שיווקי', '2.0.6', 'DEV',    '2025-03-28 00:17:20', 'leumi-trade', 'Ilan G'),
+  ('אתר שיווקי', '2.0.3', 'TEST',   '2025-03-28 00:07:25', 'leumi-trade', 'Ilan G'),
+  ('עסקים אתר', '2.0.5', 'DEV',    '2025-03-28 00:17:20', 'digital-business', 'Doron T'),
+  ('עסקים אפליקציה', '3.0.3', 'DEV',    '2025-03-28 07:17:20', 'digital-business', 'Amir M'),
+  ('זיהוי הונאה', '3.9.7', 'PROD',   '2025-03-28 09:17:20', 'digital-business', 'Amir M');
