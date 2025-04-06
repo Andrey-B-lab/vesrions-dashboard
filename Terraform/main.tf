3a4d6702ad415c02f0d58b1266d194a2d0408831
Andrey B <72738126+Andrey-B-lab@users.noreply.github.com>
2025-04-06 20:19:35 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
-provider "aws" {        # provider definition
-  region = "us-east-1"  # the region I run the EC2 instance
-}
-
 data "aws_vpc" "default" {
   default = true
+}
+
+provider "aws" {        # provider definition
+  region = "us-east-1"  # the region I run the EC2 instance
 }
